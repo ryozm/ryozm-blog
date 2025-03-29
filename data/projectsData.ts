3abac1d2b6edd4fef13c9c744ffb309235ca65f6
ryozm <ryozming@gmail.com>
2025-03-29 14:59:12 +0800

    feat(projectsData): 更新项目数据

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,21 +7,20 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'MachineLink+',
+    description: `Close to business scenarios,  a user-friendly and
+powerful tool for machine management.`,
+imgSrc: '/static/images/time-machine.jpg',
+    href: 'https://en.rootcloud.com/solutions-products/rootcloud-machinelink.html',
   },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
+  // {
+  //   title: 'The Time Machine',
+  //   description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+  //   to the desired date and press "Go". No more worrying about lost keys or
+  //   forgotten headphones with this simple yet affordable solution.`,
+  //   imgSrc: '/static/images/time-machine.jpg',
+  //   href: '/blog/the-time-machine',
+  // },
 ]
 
 export default projectsData
