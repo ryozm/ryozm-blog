585ea566c3fb496ea00c3fa5ddac66dbc4ea813b
ryozm <ryozming@gmail.com>
2025-04-03 23:46:31 +0800

    fix(next.config.js): 添加codesandbox.io到frame-src白名单

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const ContentSecurityPolicy = `
   media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
-  frame-src giscus.app
+  frame-src giscus.app codesandbox.io
 `
 
 const securityHeaders = [
@@ -83,7 +83,7 @@ module.exports = () => {
         {
           protocol: 'https',
           hostname: 'img.shields.io',
-        }
+        },
       ],
       unoptimized,
     },
